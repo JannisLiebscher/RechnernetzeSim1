da9117a9280908552973d0a1d6642ed1a57018e8
Jannis Liebscher <jannis-liebscher@gmx.de>
2022-10-22 21:32:41 +0200

    Bisle weitergemacht

--- Simulation.py
+++ Simulation.py
@@ -54,7 +54,7 @@ class Ev:
 # methods push, pop, and start as described in the problem description
 
 class EvQueue:
-    liste = heapq()
+    liste = heapq
     evCount = 0
     time = 0
     def push(event):
@@ -78,27 +78,33 @@ class Station():
         self.stationsnamename = name
 
     def anstellen(self, customer):
-        if (customer.shoppinglist[0][2] <= len(self.buffer)):
+        if (Customer.nextStationMaxQueue(customer) <= len(self.buffer)):
             if (len(self.buffer) == 0 and self.busy == False):
                 self.buffer.append(customer)
                 self.busy = True
-                fertig = Ev(EvQueue.time + (self.delay_per_Item * customer.shoppinglist[0][3]), self.stationsname, (customer, "Fertig"), 1) # 1 eventuell weg
+                fertig = Ev(EvQueue.time + (self.delay_per_Item * Customer.nextStationItemCount(customer)),
+                            self.stationsname,
+                            (customer, "Fertig"),
+                            1) # 1 eventuell weg
                 EvQueue.push(fertig)
             else:
                 self.buffer.append(customer)
         else:
-            customer.shoppinglist.remove(0)
+            Customer.removeCurrentListItem(customer)
 
     def fertig(self):
         self.busy = False
-        self.buffer.popleft()
+        customerDone = self.buffer.popleft()
         if (len(self.buffer) > 0):
             self.busy = True
-            fertig = Ev(EvQueue.time + (self.delay_per_Item * self.buffer[0].shoppinglist[0][3]), self.stationsname,
-                        (self.buffer[0], "Fertig"), 1)  # 1 eventuell weg
+            fertig = Ev(EvQueue.time + (self.delay_per_Item * Customer.nextStationItemCount(self.buffer[0])),
+                        self.stationsname,
+                        (self.buffer[0], "Fertig"),
+                        1)  # 1 eventuell weg
             EvQueue.push(fertig)
-        
-        
+        anstellen = Ev(EvQueue.time + (customerDone.shoppinglist[0][0]), customerDone.shoppinglist[2],
+                    (customerDone, "Ankunft"), 1)  # 1 eventuell weg
+        EvQueue.push(anstellen)
 
 
 
@@ -116,14 +122,26 @@ class Customer():
     duration = 0
     duration_cond_complete = 0
     count = 0
-# please implement here
 
     def __init__(self, shoppinglist, id, time):
         self.shoppinglist = shoppinglist
         self.id = id
         self.time = time
 
+    def nextStationWalkTime(self):
+        return self.shoppinglist[0][0]
 
+    def nextStation(self):
+        return self.shoppinglist[0][1]
+
+    def nextStationItemCount(self):
+        return self.shoppinglist[0][2]
+
+    def nextStationMaxQueue(self):
+        return self.shoppinglist[0][3]
+
+    def removeCurrentListItem(self):
+        self.shoppinglist.remove(0)
 
 def startCustomers(einkaufsliste, name, sT, dT, mT):
     i = 1
@@ -149,6 +167,7 @@ Customer.dropped['Bäcker'] = 0
 Customer.dropped['Metzger'] = 0
 Customer.dropped['Käse'] = 0
 Customer.dropped['Kasse'] = 0
+# WalkTime - Name - ItemCount - MaxQueue
 einkaufsliste1 = [(10, baecker, 10, 10), (30, metzger, 5, 10), (45, kaese, 3, 5), (60, kasse, 30, 20)] # 3 überspringen
 einkaufsliste2 = [(30, metzger, 2, 5), (30, kasse, 3, 20), (20, baecker, 3, 20)]
 startCustomers(einkaufsliste1, 'A', 0, 200, 30 * 60 + 1)
